d0982a68fd8db5f9c5dd233b92e7943888b99703
MATVEYKAO4EN <zoomwest@mail.ru>
2025-03-01 13:14:40 +0300

    done

---
+++ main.py
@@ -0,0 +1,100 @@
+import clickhouse_connect
+
+def connect(host,port,username,password):
+    client = clickhouse_connect.get_client(host=host, port=port, username=username, password=password)
+    return client
+
+def createDBandTable(client,db,table):
+    client.command(f'CREATE DATABASE IF NOT EXISTS {db}')
+
+    create_table_query = f'''
+    CREATE TABLE IF NOT EXISTS {db}.{table} (
+        wordform String,
+        amt UInt32
+    ) ENGINE = MergeTree
+    ORDER BY tuple();
+    '''
+    client.command(create_table_query)
+
+def insert(client, file_paths, db, table):
+    batch_size = 10000  # Размер батча
+
+    # Функция для вставки батча
+    def insert_batch(batch):
+        if batch:
+            client.insert(f'{db}.{table}', batch, column_names=['wordform', 'amt'])
+
+    # Чтение файлов и вставка данных
+    for file_path in file_paths:  
+        batch = []
+        
+        with open(file_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                    
+                line = line.replace('(', ' ').replace(')', ' ')
+                wordforms = line.strip().split()
+                batch.extend([(wordform, 1) for wordform in wordforms])
+
+                # Если батч достиг размера batch_size, вставляем его
+                if len(batch) >= batch_size:
+                    insert_batch(batch)
+                    batch = []  # Очищаем батч после вставки
+
+            # Вставляем оставшиеся данные (последний батч)
+            if batch:  # Проверяем, что батч не пустой
+                insert_batch(batch)
+    print("Все файлы успешно вставлены.")
+
+def count(client,db,table):
+    # SQL-запрос для группировки и суммирования
+    query = f"""
+    SELECT wordform, SUM(amt) AS total_amt
+    FROM {db}.{table}
+    GROUP BY wordform
+    """
+
+    # Выполнение запроса
+    result = client.query(query)
+
+    # Получение результатов
+    grouped_data = result.result_set
+
+    # Удаление старых данных и вставка новых
+    client.command(f"TRUNCATE TABLE {db}.{table}")
+
+    insert_query = f"INSERT INTO {db}.{table} (wordform, amt) VALUES"
+    values = [(row[0], row[1]) for row in grouped_data]
+    client.insert(f"{db}.{table}", values, column_names=['wordform', 'amt'])
+
+    print("Данные успешно обновлены.")
+
+def clearTable(db,table):
+   # Очистка таблицы
+   client.command(f'TRUNCATE TABLE {db}.{table}')
+   print(f"Таблица {db}.{table} успешно очищена.")
+
+if __name__ == "__main__":
+    host ='localhost'
+    port='8123'
+    username='default'
+    password=''
+    database='DBWordForm'
+    table='form_table'
+    file_paths = [
+    r'C:\Users\AAKuk\Desktop\matvey_c++\clickhousePy\wordList.txt'
+]
+    # Подключение к ClickHouse
+    client = connect(host, port, username, password)
+    
+    # # Создание базы данных и таблицы
+    # createDBandTable(client, database, table)
+
+
+    # Вставка данных из файла
+    insert(client, file_paths, database, table)
+    
+    # Подсчёт количества строк в таблице
+    count(client, database, table)
+
+    # #Очистить таблицу
+    # clearTable(client,database,table)
