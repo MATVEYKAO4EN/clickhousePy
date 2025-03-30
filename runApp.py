e8c5fdc6abb6b9ecef11142cc354920f7deaed2c
MATVEYKAO4EN <zoomwest@mail.ru>
2025-03-30 11:49:50 +0300

    update

--- runApp.py
+++ runApp.py
@@ -30,7 +30,6 @@ def save_config():
     }
     with open("config.json", "w", encoding="utf-8") as f:
         json.dump(config, f, indent=4, ensure_ascii=False)
-    print("Данные сохранены в config.json")
     
 
 file_paths = []
